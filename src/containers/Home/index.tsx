d88981c980eec4fd8c2288336adfbba482e1d22c
ly2011 <1518550424@qq.com>
2018-01-04 13:11:08 +0800

    解决typescript识别css文件，定义declaration.d.ts

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-
+import styles from './index.module.scss';
 function Home() {
   return (
     <div className="layout">
-      <h1>这是Home页面</h1>
+      <h1 className={styles.title}>这是Home页面</h1>
     </div>
   );
 }
